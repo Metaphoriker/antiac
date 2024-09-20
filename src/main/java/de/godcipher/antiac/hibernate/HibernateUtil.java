01242eeb3cec86b526e02762592f2ab88d18d1cc
GodCipher <luziferisdoof@gmail.com>
2024-09-20 14:17:59 +0200

    refact: Bettered up logging in HibernateUtil

--- src/main/java/de/godcipher/antiac/hibernate/HibernateUtil.java
+++ src/main/java/de/godcipher/antiac/hibernate/HibernateUtil.java
@@ -1,11 +1,13 @@
 package de.godcipher.antiac.hibernate;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+@Slf4j
 public class HibernateUtil {
 
   @Getter private static SessionFactory sessionFactory;
@@ -17,7 +19,7 @@ public class HibernateUtil {
           new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
       sessionFactory = configuration.buildSessionFactory(serviceRegistry);
     } catch (Exception e) {
-      e.printStackTrace();
+      log.error("Initial SessionFactory creation failed", e);
       throw new ExceptionInInitializerError("Initial SessionFactory creation failed" + e);
     }
   }
